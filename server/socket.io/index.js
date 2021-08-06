541fc1ae2248ad360f35651c911d705c0f8873b7
Anton <a.kuchmiy19@gmail.com>
2021-08-06 17:22:26 +0300

    on-connection socket io handler renamed

--- server/socket.io/index.js
+++ server/socket.io/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const app = require('../express')
 const server = http.createServer(app)
 const options = require('../config/io.config')
-const registerOnConnection = require('./handlers')
+const registerOnConnection = require('./handlers/on-connection')
 
 const io = require('socket.io')(server, options)
 
