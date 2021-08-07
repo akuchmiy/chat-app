0d343443510506dc258e8d26f1f3e0d36a8638ae
Anton <a.kuchmiy19@gmail.com>
2021-08-07 18:46:06 +0300

    resource middleware for user router

--- server/express/routes/users/users.controller.js
+++ server/express/routes/users/users.controller.js
@@ -16,16 +16,7 @@ const userController = {
   // route: GET /users/:userID
   async getUser(req, res) {
     try {
-      const userId = req.params.userId
-      const user = await User.findOne({ _id: userId }, [
-        '_id',
-        'username',
-        'rooms',
-      ])
-      if (!user)
-        return res
-          .status(400)
-          .json({ message: 'There is no user with such id', data: null })
+      const user = req.resource
 
       return res.status(200).json({ data: user })
     } catch (e) {
@@ -34,12 +25,7 @@ const userController = {
   },
   async getUserRooms(req, res) {
     try {
-      const userId = req.params.userId
-      const user = await User.findOne({ _id: userId }, ['_id', 'rooms'])
-      if (!user)
-        return res
-          .status(400)
-          .json({ message: 'There is no user with such id', data: null })
+      const user = req.resource
 
       if (!user.rooms)
         return res.status(204)
--- server/express/routes/users/users.router.js
+++ server/express/routes/users/users.router.js
@@ -1,12 +1,22 @@
 const { Router } = require('express')
 const authMiddleware = require('../../middleware/authMiddleware')
+const resourceMiddleware = require('../../middleware/resourceMiddleware')
+const {User} = require('../../../models')
 const controller = require('./users.controller')
+
+function userResourceMiddleware(fieldsToRetrieve) {
+  return resourceMiddleware('user', User, 'userId', fieldsToRetrieve)
+}
 
 const router = Router()
 
 router.use(authMiddleware)
 router.get('/', controller.getAllUsers)
-router.get('/:userId', controller.getUser)
-router.get('/:userId/rooms', controller.getUserRooms)
+router.get('/:userId', [
+  userResourceMiddleware('_id username rooms')
+], controller.getUser)
+router.get('/:userId/rooms', [
+  userResourceMiddleware('_id rooms')
+], controller.getUserRooms)
 
 module.exports = router
