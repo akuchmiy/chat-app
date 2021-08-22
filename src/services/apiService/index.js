8e888ad4a0f21b5d2941767f53c369f808db5fac
Anton <a.kuchmiy19@gmail.com>
2021-08-22 21:08:44 +0300

    different server url on dev and prod

--- src/services/apiService/index.js
+++ src/services/apiService/index.js
@@ -3,8 +3,10 @@ import authApiService  from './authApiService'
 import roomApiService from './roomApiService'
 import userApiService from './userApiService'
 
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://192.168.50.251:3000' : window.location.origin
+
 const apiClient = axios.create({
-  baseURL: 'http://192.168.50.251:3000',
+  baseURL,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,6 +1,7 @@
 import {io} from 'socket.io-client'
 
-export const socket = io('http://192.168.50.251:3000')
+const url = process.env.NODE_ENV === 'development' ? 'http://192.168.50.251:3000' : window.location.origin
+export const socket = io(url)
 
 socket.on('connect', () => {
   console.log("Socket connected")
